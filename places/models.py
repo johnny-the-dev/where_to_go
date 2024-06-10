bb6d235807a79df670227838d5393c86c67c6321
Ivan Arkhipov <vanchez666@gmail.com>
2024-06-10 22:55:43 +0300

    add places, placeimage, placecoordinate models

--- places/models.py
+++ places/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class Place(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description_short = models.CharField(max_length=200)
+    description_long = models.TextField()
+
+    def __str__(self):
+        return str(self.title)
+
+
+class PlaceImage(models.Model):
+    url = models.URLField()
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        related_name='imgs'
+    )
+
+    def __str__(self) -> str:
+        return str(self.url)
+
+
+class PlaceCoordinate(models.Model):
+    lng = models.FloatField()
+    lat = models.FloatField()
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='coords'
+    )
