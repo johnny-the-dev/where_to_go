e59308216a24bf780dbc5943066bbae0d1c139ea
Ivan Arkhipov <vanchez666@gmail.com>
2024-06-11 19:21:48 +0300

    Add image model

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
+from django.db.models.functions import Concat
 
-from .models import Place
+from .models import Place, PlaceCoordinate, PlaceImage
 
-# Register your models here.
-admin.site.register(Place)
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 1
+    classes = ("collapse",)
+
+
+class PlaceCoordinateInline(admin.StackedInline):
+    model = PlaceCoordinate
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    ordering = ('title',)
+    inlines = [PlaceImageInline, PlaceCoordinateInline]
+
+    def get_list_display_links(self, request, list_display):
+        return list_display
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    ordering = ('place__title', 'number')
+
+    @admin.display(description='локация', ordering=Concat('place__title', 'number'))
+    def title(self, obj):
+        return f'{obj.number} {obj.place.title}'
+
+    def get_list_display_links(self, request, list_display):
+        return list_display
--- places/models.py
+++ places/models.py
@@ -7,28 +7,37 @@ class Place(models.Model):
     description_short = models.CharField(max_length=200)
     description_long = models.TextField()
 
+    class Meta:
+        verbose_name = "Локация"
+        verbose_name_plural = "Локации"
+
     def __str__(self):
         return str(self.title)
 
 
 class PlaceImage(models.Model):
-    url = models.URLField()
-    place = models.ForeignKey(
-        Place,
-        on_delete=models.CASCADE,
-        related_name='imgs'
-    )
+    number = models.IntegerField(verbose_name="Номер картинки", null=True, blank=True)
+    image = models.ImageField(upload_to="images/", verbose_name="Изображение")
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="images", verbose_name='Локация')
+
+    class Meta:
+        verbose_name = "Изображение"
+        verbose_name_plural = "Изображения"
 
     def __str__(self) -> str:
-        return str(self.url)
+        return f'{self.number or ""} "{self.place.title}"'
 
 
 class PlaceCoordinate(models.Model):
-    lng = models.FloatField()
-    lat = models.FloatField()
+    lng = models.CharField(max_length=10)
+    lat = models.CharField(max_length=10)
     place = models.OneToOneField(
-        Place,
-        on_delete=models.CASCADE,
-        primary_key=True,
-        related_name='coords'
+        Place, on_delete=models.CASCADE, primary_key=True, related_name="coords"
     )
+
+    class Meta:
+        verbose_name = "Координаты"
+        verbose_name_plural = "Координаты"
+
+    def __str__(self):
+        return f'"{self.place.title}"'
