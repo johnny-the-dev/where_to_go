5ab9497d5d29964b811d81e4e7e86f5587a51735
Ivan Arkhipov <vanchez666@gmail.com>
2024-06-23 20:50:21 +0300

    refactor URL routing for 'tinymce' to the base url file

--- places/urls.py
+++ places/urls.py
@@ -1,9 +1,8 @@
-from django.urls import include, path
+from django.urls import path
 
 from places import views
 
 urlpatterns = [
     path('', views.index),
-    path('tinymce/', include('tinymce.urls')),
     path('places/<slug:place_id>/', views.place_detail_view, name='place_detail'),
 ]
